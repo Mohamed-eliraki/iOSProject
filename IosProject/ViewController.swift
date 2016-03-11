c335c95c31e576703970a9f218e735f069a25b70
tp <tp@tps-macbook-pro.local>
2016-03-11 12:21:39 +0100

    ffff

--- IosProject/ViewController.swift
+++ IosProject/ViewController.swift
@@ -17,10 +17,6 @@ class ViewController: UIViewController {
        print("ssss ddd")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
